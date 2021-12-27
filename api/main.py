23fe50ce3d8724fa753940f0fd50d0a606da3799
Diego Ortiz <dxortiz2@gmail.com>
2021-12-27 14:07:50 -0600

    images endpoint get post

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
 import os
 import requests
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from mongo_client import mongo_client
+
+db = mongo_client.react
+react_collection = db.images
+
+load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL="https://api.unsplash.com/photos/random"
 UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
@@ -28,6 +34,21 @@ def new_image():
     
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        # read images from db
+        images = react_collection.find({})
+        return jsonify([img for img in images])
+    if request.method == "POST":
+        # save image in db
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = react_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
