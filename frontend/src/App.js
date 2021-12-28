a58b5a8a028c90b100710e4c86e7d41b4da42de3
Diego Ortiz <dxortiz2@gmail.com>
2021-12-28 10:01:10 -0600

    axios conversion and load images from db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
 import { Container, Row, Col } from 'react-bootstrap';
 import Welcome from './components/Welcome';
+import axios from 'axios';
 
 // const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -15,21 +16,29 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
-  console.log(images);
+  const getSavedImages = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/images`);
+      setImages(res.data || [])
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const handleSearchSubmit = (e) => {
+  useEffect(() => getSavedImages(), []);
+
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    fetch(`${API_URL}/new-image?query=${word}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setImages([{ ...data, title: word }, ...images])
 
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`)
+      setImages([{ ...res.data, title: word }, ...images])
+    } catch (error) {
+      console.log(error)
+    }
     setWord('');
   };
+
 
   const handleDeleteImage = (id) => {
     setImages(images.filter((image) => image.id !== id));
